550e12664b57bf5bddb3199b00cf9bbe56d2c419
Mariela Nabia <marielanabia.developer@gmail.com>
2022-07-29 09:22:10 -0300

    carpeta backend

---
+++ Backend/src/main/java/com/portfolio/MarielaNabia/MarielaNabiaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.MarielaNabia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarielaNabiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarielaNabiaApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/portfolio/MarielaNabia/MarielaNabiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.MarielaNabia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarielaNabiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
